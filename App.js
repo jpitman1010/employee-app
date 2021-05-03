629b750e78aa68ba12846ceb6ec59c1bd5577da6
jpitman1010 <jpitman1010@gmail.com>
2021-05-03 14:54:49 -0700

    added multiple screens,

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import Home from './screens/Home';
+import CreateEmployee from './screens/CreateEmployee';
+import Profile from './screens/Profile';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Home/> */}
+      {/* <CreateEmployee /> */}
+      <Profile />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +22,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
