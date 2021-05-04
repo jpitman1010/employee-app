f2f7b3fc9b945ca25418a60c0f7b74d4f91cce2e
jpitman1010 <jpitman1010@gmail.com>
2021-05-04 13:15:35 -0700

    set up MongoDb Cluster to app

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+
+const mongoUri = 'mongodb+srv://WirlixEmployeeApp:WirlixDev1@cluster0.gbo7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 app.get('/',(req,res)=>{
     res.send('welcome to node js')
 })
