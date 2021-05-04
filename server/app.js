298192334e18d577210578f6f849a740bd75efbd
jpitman1010 <jpitman1010@gmail.com>
2021-05-04 12:56:44 -0700

    starting to add  backend mongo, node js'

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+app.get('/',(req,res)=>{
+    res.send('welcome to node js')
+})
+
+
+app.listen(3000),()=>{
+    console.log('server running')
+}
