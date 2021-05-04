cab687b521e0148fefa353ce0d7985e3eb2552f8
jpitman1010 <jpitman1010@gmail.com>
2021-05-04 14:06:42 -0700

    successfully made post data route handler

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('./Employee')
 
+app.use(bodyParser.json())
+
 const Employee = mongoose.model('employee')
 
 const mongoUri = 'mongodb+srv://WirlixEmployeeApp:WirlixDev1@cluster0.gbo7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -22,6 +24,24 @@ mongoose.connection.on('error',(err)=>{
 
 app.get('/',(req,res)=>{
     res.send('welcome to node js')
+})
+
+app.post('/send-data',(req,res)=>{
+    const employee = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        profilePicture: req.body.profilePicture,
+        email: req.body.email,
+        phone: req.body.phone,
+        salary: req.body.salary
+    })
+    employee.save()
+    .then(data=>{
+        console.log(data)
+        res.send('posted successfully')
+    }).catch(err=>{
+        console.log(err)
+    })
 })
 
 
