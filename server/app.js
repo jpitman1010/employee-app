1bae0ba8b3e59387965fb1318fd671453dcd8f2e
jpitman1010 <jpitman1010@gmail.com>
2021-05-04 13:27:39 -0700

    successful connection from app to mongodb'

--- server/app.js
+++ server/app.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+require('./Employee')
 
+const Employee = mongoose.model('employee')
 
 const mongoUri = 'mongodb+srv://WirlixEmployeeApp:WirlixDev1@cluster0.gbo7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log('connected to mongo, yeah!!!')
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('error connecting', err)
+})
 
 app.get('/',(req,res)=>{
     res.send('welcome to node js')
